b185db1959fad9651ee3fb8f1ee5e272a99e43fb
ohyoungkyu <come8908@gmail.com>
2022-10-07 13:18:55 +0900

    리액트 라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React from "react";
+
 function App() {
   return (
-    <div className="bg-red-500">
-      tailwind<button className="btn">Button</button>
+    <div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import About from "./Routes/About";
+import Home from "./Routes/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div className="flex gap-2">
+        <span className="btn">
+          <Link to="/">App</Link>
+        </span>
+        <span className="btn">
+          <Link to="About">About</Link>
+        </span>
+        <span className="btn">
+          <Link to="/Home">Home</Link>
+        </span>
+      </div>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
