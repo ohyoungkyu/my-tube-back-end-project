fb59f050d7895d3bb16e3d690adb82847fd02f2b
ohyoungkyu <come8908@gmail.com>
2022-10-12 11:49:59 +0900

    헤더 제작2

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,55 +5,55 @@ const Header = () => {
   const navigate = useNavigate();
   return (
     <header>
-      <div className="navbar bg-base-100 border-b shadow-md">
-        <div className="navbar-start">
+      <div className="flex justify-between navbar bg-base-100 border-b shadow-md">
+        <div>
           <div
             className="btn btn-ghost normal-case text-xl hover:bg-transparent"
             onClick={() => {
               navigate("/");
             }}
-          >    
-              <i className="fi fi-brands-youtube text-red-500 mt-2"></i>
-              <div className="ml-2">MyTube</div>
+          >
+            <i className="fi fi-brands-youtube text-red-500 mt-2"></i>
+            <div className="ml-2">MyTube</div>
           </div>
         </div>
-        <div className="navbar-center"></div>
-        <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+        <div className="flex-1 justify-end gap-2 ml-auto">
+          <div className="form-control mx-auto w-1/3">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered border-gray-400"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" class="btn btn-ghost btn-circle avatar">
+              <div class="w-24 rounded-full">
+                <img src="https://placeimg.com/192/192/people" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
+              <li>
+                <div>
+                  <Link to="/about/3">Profile</Link>
+                </div>
+              </li>
+              <li>
+                <a>Setting</a>
+              </li>
+              <li>
+                <div onClick={() => {
+                  navigate("/");
+                }}>
+                  Logout
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
