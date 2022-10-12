aac2759aef533d115aec6c7dd104387bd9ec3081
ohyoungkyu <come8908@gmail.com>
2022-10-12 11:28:05 +0900

    헤더 제작1

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -4,7 +4,7 @@ import Layout from "../layouts/Layout";
 const Home = () => {
   return (
     <Layout>
-      <div>home</div>;
+      <div>home</div>
     </Layout>
   )
 };
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,51 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 border-b shadow-md">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <div>
-                  <Link to="/">Homepage</Link>
-                </div>
-              </li>
-              <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <div>
-                  <Link to="/about/3">About</Link>
-                </div>
-              </li>
-            </ul>
+          <div
+            className="btn btn-ghost normal-case text-xl hover:bg-transparent"
+            onClick={() => {
+              navigate("/");
+            }}
+          >    
+              <i className="fi fi-brands-youtube text-red-500 mt-2"></i>
+              <div className="ml-2">MyTube</div>
           </div>
         </div>
-        <div className="navbar-center">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
+        <div className="navbar-center"></div>
         <div className="navbar-end">
           <button className="btn btn-ghost btn-circle">
             <svg
