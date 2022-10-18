922ed5675ae59e1a89b34a1a11e0b7ef1962b9ef
ohyoungkyu <come8908@gmail.com>
2022-10-18 13:36:33 +0900

    pc와 pc 불러오는 중 문제발생

--- src/Routes/studio/Channel.jsx
+++ src/Routes/studio/Channel.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import VideoList from "../../components/studio/channel/VideoList";
 import Layout from "../../layouts/Layout";
 
 const Channel = () => {
   return (
     <Layout>
-      <div>Channel</div>
+      <div className="flex pointer-event-auto">
+        <div className="flex w-60 flex-shrink-0 border border-r-0 border-t-0 z-0">
+          side menu
+        </div>
+        <div className="flex flex-col flex-grow z-0 border border-b-0">
+          <VideoList />
+        </div>
+      </div>
     </Layout>
   );
 };
